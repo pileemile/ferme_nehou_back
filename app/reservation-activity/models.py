ddcb32841e7da09aad116a83704116b2473bb738
emile <emilepicard323@gmail.com>
2025-11-05 15:28:36 +0100

    add reservations_activity

---
+++ app/reservation-activity/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from app.activity.models import Activity
+from app.reservation.models import Reservation
+
+
+class ReservationActivity(models.Model):
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
