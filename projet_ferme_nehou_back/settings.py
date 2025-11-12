741feee67c8f649a8091d336618b2a6478010cf2
emile <emilepicard323@gmail.com>
2025-11-12 15:13:35 +0100

    filter django and update gitignore

--- projet_ferme_nehou_back/settings.py
+++ projet_ferme_nehou_back/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_spectacular',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
