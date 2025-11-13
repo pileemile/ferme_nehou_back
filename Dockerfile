b5fc4b168a97bc02decf1d795ac7d5a158b0d91f
emile <emilepicard323@gmail.com>
2025-11-13 11:59:42 +0100

    icon + fixtures

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,22 @@
 FROM python:3.13
 
-# Installer les dépendances système nécessaires
 RUN apt-get update && apt-get install -y \
     libpq-dev \
     build-essential \
     zsh
 
-# Définir le répertoire de travail
 WORKDIR /app
 
-# Copier requirements.txt et installer les dépendances Python
 COPY requirements.txt /app/
 
-# Installer Oh My Zsh
 RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Installer les dépendances Python
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copier le reste du code
+RUN mkdir -p /app/media
+
 COPY . /app/
 
-# Exposer le port 8080
 EXPOSE 8080
 
-# Commande par défaut
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8080"]
--- app/rooms/models.py
+++ app/rooms/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+
+'''
+Tu pourras ensuite créer des amenities comme :
+| name         | icon                       |
+| ------------ | -------------------------- |
+| Wi-Fi        | fa-solid fa-wifi           |
+| Jacuzzi      | fa-solid fa-hot-tub-person |
+| TV           | fa-solid fa-tv             |
+| Kitchen      | fa-solid fa-kitchen-set    |
+| Free Parking | fa-solid fa-square-parking |
+
+'''
+
+
+class AmenityModel(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=50, blank=True, null=True)
+
+
 class RoomModel(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
@@ -7,3 +26,4 @@ class RoomModel(models.Model):
     price_per_night = models.DecimalField(max_digits=7, decimal_places=2)
     available = models.BooleanField(default=True)
     main_photo = models.ImageField(upload_to='room_photos/', blank=True, null=True)
+    amenities = models.ManyToManyField('AmenityModel', related_name='rooms', blank=True)
