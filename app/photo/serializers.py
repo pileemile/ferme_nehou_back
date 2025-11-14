1900522f97b9be3936ec4c4d03fd67423b4a538c
emile <emilepicard323@gmail.com>
2025-11-14 11:00:07 +0100

    photo

--- app/photo/serializers.py
+++ app/photo/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
+from drf_spectacular.types import OpenApiTypes
 from .models import Photo
 
-
 class SerializerPhoto(serializers.ModelSerializer):
+    file_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Photo
         fields = [
             'id',
             'room',
+            'file',
             'file_url',
             'description',
         ]
+
+    @extend_schema_field(OpenApiTypes.STR)
+    def get_file_url(self, obj):
+        request = self.context.get('request')
+        if obj.file and request:
+            return request.build_absolute_uri(obj.file.url)
+        return None
--- projet_ferme_nehou_back/settings.py
+++ projet_ferme_nehou_back/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import environ
-
+from django.template.context_processors import media
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -22,6 +22,11 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
