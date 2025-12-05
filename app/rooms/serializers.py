bd20c4d564d0a3d6a5f9cbae7921ac8be83e5b1e
emile <emilepicard323@gmail.com>
2025-12-05 15:25:18 +0100

    add amenities in rooms

--- app/rooms/serializers.py
+++ app/rooms/serializers.py
@@ -4,7 +4,19 @@ from drf_spectacular.types import OpenApiTypes
 from app.rooms.models import RoomModel, AmenityModel
 
 
+class AmenitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AmenityModel
+        fields = [
+            'id',
+            'name',
+            'icon'
+        ]
+
+
 class SerializerRooms(serializers.ModelSerializer):
+    amenities = AmenitySerializer(many=True, read_only=True)
+
     class Meta:
         model = RoomModel
         fields = [
@@ -15,19 +27,11 @@ class SerializerRooms(serializers.ModelSerializer):
             'price_per_night',
             'available',
             'main_photo',
-        ]
+            'amenities'
+    ]
 
     @extend_schema_field(OpenApiTypes.STR)
     def get_main_photo(self, obj):
         if obj.main_photo:
             return self.context['request'].build_absolute_uri(obj.main_photo.url)
         return None
-
-class AmenitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AmenityModel
-        fields = [
-            'id',
-            'name',
-            'icon'
-        ]
